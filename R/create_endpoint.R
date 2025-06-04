f2798212931113ca17adb6a1cabf93f294be69cb
julianflowers12 <julian.flowers13@gmail.com>
2025-06-04 10:28:46 +0100

    added functions

---
+++ R/create_endpoint.R
@@ -0,0 +1,18 @@
+#' Create an endpoint for fetching consumption data from Octopus Energy API
+#' @param mpan A string representing the MPAN (Meter Point Administration Number).
+#' @param serial A string representing the meter serial number.
+#' @return A string representing the complete URL endpoint for fetching consumption data.
+#' @examples
+#' # create_endpoint("1234567890123", "ABC123456789")
+#' @export
+
+
+
+
+create_endpoint <- function(mpan, serial){
+    base_url <- "https://api.octopus.energy/v1/"
+
+    url1 <- paste0(base_url, "electricity-meter-points/", mpan, "/meters/", serial, "/consumption/?page_size=50000")
+
+    return(url1)
+}
