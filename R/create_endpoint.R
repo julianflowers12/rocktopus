974fdebf82b1233f7fc5ec83eeee4112d0aa37fd
julianflowers12 <julian.flowers13@gmail.com>
2025-06-05 19:21:58 +0100

    update

--- R/create_endpoint.R
+++ R/create_endpoint.R
@@ -12,7 +12,7 @@
 create_endpoint <- function(mpan, serial){
     base_url <- "https://api.octopus.energy/v1/"
 
-    url1 <- paste0(base_url, "electricity-meter-points/", mpan, "/meters/", serial, "/consumption/?page_size=50000")
+    url1 <- paste0(base_url, "electricity-meter-points/", mpan, "/meters/", serial, "/consumption/?page_size=25000&period_from=2024-01-01T00:00:00")
 
     return(url1)
 }
