63ea94b63f8173479f16d320780abafe009bd25f
julianflowers12 <julian.flowers13@gmail.com>
2025-06-07 08:26:52 +0100

    updates

--- R/create_endpoint.R
+++ R/create_endpoint.R
@@ -2,9 +2,12 @@
 #' @param mpan A string representing the MPAN (Meter Point Administration Number).
 #' @param serial A string representing the meter serial number.
 #' @return A string representing the complete URL endpoint for fetching consumption data.
-#' @examples
-#' # create_endpoint("1234567890123", "ABC123456789")
+#' @details
+#' The function constructs a URL that can be used to fetch consumption data for a specific electricity meter point.
+#' The URL includes parameters for pagination and a specific time period.
+#' @examples# create_endpoint("1234567890123", "ABC123456789")
 #' @export
+#'
 
 
 
